199095ec9f3a2ad895132cdf3a402f5194cfe966
programmingd32@gmail.com <programmingd32@gmail.com@409a0e01-0899-c390-7acb-22762e678f91>
2015-01-25 05:06:15 +0000

    cap nhat freaky

--- TMP-BrainGame/src/tmp/braingame/game/FreakyShapeGame.java
+++ TMP-BrainGame/src/tmp/braingame/game/FreakyShapeGame.java
@@ -7,6 +7,7 @@ import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.sprite.ButtonSprite;
+import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.text.Text;
 import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
@@ -31,9 +32,11 @@ public class FreakyShapeGame extends GameBaseScene implements IOnSceneTouchListe
     private ButtonSprite button2;
     private ITextureRegion button1texture;
     private ITextureRegion button2texture;
+    private Sprite shape1,shape2;
     private BitmapTextureAtlas buildBmp;
     private Text mQuestion;
     private GameData gameData;
+
 
     private Background createFromRGB(float r,float g,float b){
         return new Background(r/0xff,g/0xff,b/0xff);
@@ -45,7 +48,6 @@ public class FreakyShapeGame extends GameBaseScene implements IOnSceneTouchListe
     @Override
     public void createScene() {
         createBackground();
-        createGameData();
         createHUD();
         this.setTouchAreaBindingOnActionDownEnabled(true);
         this.registerTouchArea(button1);
@@ -53,6 +55,7 @@ public class FreakyShapeGame extends GameBaseScene implements IOnSceneTouchListe
         attachChild(button1);
         attachChild(button2);
         attachChild(mQuestion);
+        createGameData();
     }
 
     private void createGameData() {
@@ -62,6 +65,18 @@ public class FreakyShapeGame extends GameBaseScene implements IOnSceneTouchListe
             public void onChanged() {
                 mQuestion.setText(gameData.getQuestion());
                 mQuestion.setPosition(20+mQuestion.getWidth()/2,400);
+                String file1=gameData.getFirstAnswerFileName();
+                String file2=gameData.getSecondAnswerFileName();
+                /*if (shape1!=null){
+                    button1.detachChild(shape1);
+                    button1.detachChild(shape2);
+                }
+                shape1=new Sprite(40,40,
+                        BitmapTextureAtlasTextureRegionFactory.createFromAsset(buildBmp,mActivity,file1,40,40),vbom);
+                shape2=new Sprite(40,40,
+                        BitmapTextureAtlasTextureRegionFactory.createFromAsset(buildBmp,mActivity,file2,40,40),vbom);
+                button1.attachChild(shape1);
+                button2.attachChild(shape2);*/
             }
         });
         gameData.GenerateRandomData();
@@ -87,12 +102,13 @@ public class FreakyShapeGame extends GameBaseScene implements IOnSceneTouchListe
             boolean ans=true;
             if (ans){
                 gameData.GenerateRandomData();
+
             }
         }
     };
     private void loadEntities() {
         BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/freaky/");
-        buildBmp=new BitmapTextureAtlas(mActivity.getTextureManager(),512,512);
+        buildBmp=new BitmapTextureAtlas(mActivity.getTextureManager(),1024,1024);
         button1texture=BitmapTextureAtlasTextureRegionFactory.createFromAsset(buildBmp,mActivity,"TMP.png",0,0);
         button2texture=BitmapTextureAtlasTextureRegionFactory.createFromAsset(buildBmp,mActivity,"TMP2.png",0,0);
         buildBmp.load();
@@ -106,7 +122,7 @@ public class FreakyShapeGame extends GameBaseScene implements IOnSceneTouchListe
         button2.setPosition(250 + button2.getWidthScaled()/2,20 + button2.getHeightScaled()/2);
 
         mQuestion=new Text(0, 0, mResouceManager.generalFont,
-                "Game Over!", vbom);
+                "Game Over!",100, vbom);
         mQuestion.setPosition(0,400);
     }
 
@@ -144,27 +160,56 @@ public class FreakyShapeGame extends GameBaseScene implements IOnSceneTouchListe
         private String mQuestion;
         private int mRequestShape;
         private int mRequestColor;
+        private int mShape1;
+        private int mShape2;
+        private int mColor1;
+        private int mColor2;
 
         public static final int SHAPE_COUNT=3;
-        public static final int COLOR_COUNT=3;
         public static final int SHAPE_CIRCLE=0;
         public static final int SHAPE_RECTANGLE=1;
         public static final int SHAPE_TRIANGLE=2;
 
+        public static final int COLOR_COUNT=6;
+        public static final int COLOR_WHITE=0;
+        public static final int COLOR_RED=1;
+        public static final int COLOR_PINK=2;
+        public static final int COLOR_PURPLE=3;
+        public static final int COLOR_BLUE=4;
+        public static final int COLOR_TEAL=5;
+
         private final String[] shapes={"circle","rectangle","triangle"};
+        private final String[] colors={"white","red","pink","purple","blue","teal"};
 
         private GameDataChangedListener listener;
 
         public String getQuestion(){return mQuestion;}
+        public String getFirstAnswerFileName(){return "TMP_"+colors[mColor1]+shapes[mShape1]+".png";}
+        public String getSecondAnswerFileName(){return "TMP_"+colors[mColor2]+shapes[mShape2]+".png";}
         public void setGameDataChangedListener(GameDataChangedListener listener){
             this.listener=listener;
         }
         public void GenerateRandomData(){
             Random r=new Random();
-            mRequestShape=r.nextInt(SHAPE_COUNT);
-            mRequestColor=r.nextInt(COLOR_COUNT);
+            mColor1=r.nextInt(COLOR_COUNT);
+            do{
+                mColor2=r.nextInt(COLOR_COUNT);
+            }while (mColor2==mColor1);
+            mShape1=r.nextInt(SHAPE_COUNT);
+            do{
+                mShape2=r.nextInt(SHAPE_COUNT);
+            }while (mShape2==mShape1);
+            boolean mAns=r.nextBoolean();
+            if (mAns){
+                mRequestColor=mColor1;
+                mRequestShape=mShape1;
+            } else {
+                mRequestColor=mColor2;
+                mRequestShape=mShape2;
+            }
 
-            mQuestion=shapes[mRequestShape];
+            mQuestion="Select "+colors[mRequestColor]+"\n"+shapes[mRequestShape];
+            //mQuestion=shapes[mRequestShape];
             //after generated all
             if (listener!=null)
                 listener.onChanged();
